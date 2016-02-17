2b6b362341e9b3342e5811aac108c48a1fb4cc25
svetlai <svetla_22@yahoo.com>
2016-02-17 23:20:30 +0200

    Removed platforms folder.

--- ChallengeAccepted.Mobile/platforms/android/src/main/assets/app/app.js
+++
@@ -1,8 +0,0 @@
-var application = require("application");
-application.mainModule = "./views/main/main";
-application.cssFile = "./styles/app.css";
-application.start();
-
-application.on(application.launchEvent, function (args) {
-
-});
