9b85d71e7d875745a6b7f3bcc03c97e4dc0451c2
kalindimchev <kalin.dimchev@gmail.com>
2016-02-19 08:57:14 +0200

    getting challenges http request

--- ChallengeAccepted.Mobile/app/views/challenge/challenge-to-pick.js
+++ ChallengeAccepted.Mobile/app/views/challenge/challenge-to-pick.js
@@ -3,44 +3,69 @@ var view = require("ui/core/view");
 var helperModule = require("~/common/helper");
 var observable = require("data/observable");
 var frameModule = require("ui/frame");
+var http = require("http");
+var imageSource = require("image-source");
 
 
 var challengeModules = (function() {
-	var response = [{
-	        "ChallengeId": 1,
-	        "Title": "Elbow Challenge",
-	        "Description": "Lick your elbow.",
-	        "Difficulty": 1,
-	        "DaysToComplete": 7,
-	        "CreatorName": "admin"
-	    },
-	    {
-	        "ChallengeId": 2,
-	        "Title": "Double Elbow Challenge",
-	        "Description": "Lick both your elbow.",
-	        "Difficulty": 1,
-	        "DaysToComplete": 7,
-	        "CreatorName": "admin"
-	    },
-	    {
-	        "ChallengeId": 3,
-	        "Title": "Handsfree Shot Challenge",
-	        "Description": "Drink a shot without using yor hands.",
-	        "Difficulty": 0,
-	        "DaysToComplete": 7,
-	        "CreatorName": "admin"
-	    },
-	    {
-	        "ChallengeId": 4,
-	        "Title": "Inside-out Challenge",
-	        "Description": "Wear your top/shirt inside-out in the streets.",
-	        "Difficulty": 0,
-	        "DaysToComplete": 7,
-	        "CreatorName": "admin"
-	    }]
+	var challengesResponse;
+	// var response = [{
+	//         "ChallengeId": 1,
+	//         "Title": "Elbow Challenge",
+	//         "ImageUrl": "https://eustat.files.wordpress.com/2015/05/wpid-wp-1432967860156.jpg",
+	//         "Description": "Lick your elbow.",
+	//         "Difficulty": 1,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     },
+	//     {
+	//         "ChallengeId": 2,
+	//         "Title": "Double Elbow Challenge",
+	//         "ImageUrl": "http://i.imgur.com/pV36F.gif",
+	//         "Description": "Lick both your elbow.",
+	//         "Difficulty": 1,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     },
+	//     {
+	//         "ChallengeId": 3,
+	//         "Title": "Handsfree Shot Challenge",
+	//         "ImageUrl": "http://frompghwithlove.files.wordpress.com/2011/06/19050_753784512713_8230969_42590805_7550724_n.jpg",
+	//         "Description": "Drink a shot without using yor hands.",
+	//         "Difficulty": 0,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     },
+	//     {
+	//         "ChallengeId": 4,
+	//         "Title": "Inside-out Challenge",
+	//         "ImageUrl": "http://www.nerdygaga.com/wp-content/uploads/2012/09/weird-and-funny-mens-fashion-show.jpg",
+	//         "Description": "Wear your top/shirt inside-out in the streets.",
+	//         "Difficulty": 0,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     },
+	//     {
+	//         "ChallengeId": 5,
+	//         "Title": "Lick your nose",
+	//         "ImageUrl": "http://i.dailymail.co.uk/i/pix/2015/05/08/10/2872081D00000578-3073349-image-a-8_1431077252730.jpg",
+	//         "Description": "I can't. I find that it's rather uncomfortable if I try, and my tongue ends up getting tired. But I have known people to do it.",
+	//         "Difficulty": 4,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     },
+	//     {
+	//         "ChallengeId": 6,
+	//         "Title": "Slap a cop",
+	//         "ImageUrl": "http://content4.video.news.com.au/NDM_-_news.com.au/138/929/Cop_Slap_17102012.Still004.jpg",
+	//         "Description": "Is it justified if I slap a police personnel who is in duty, for abusing me unnecessarily and slapping me?",
+	//         "Difficulty": 8,
+	//         "DaysToComplete": 7,
+	//         "CreatorName": "admin"
+	//     }];
 
 	var challenges = new observable.Observable({
-		"allChallenges": response
+		"allChallenges": challengesResponse
 	});
 
 	var topmost,
@@ -54,6 +79,8 @@ var challengeModules = (function() {
 		description,
 		rating;
 
+	var index = 0;
+
     var challengeModules = {
 
         pageLoaded: function(args) {
@@ -65,9 +92,18 @@ var challengeModules = (function() {
             descriptionLabel = view.getViewById(page, "challenge-description");
             ratingLabel = view.getViewById(page, "challenge-rating");
 
-            changeContent();
-            console.log('Title: ' + title);
-            //page.bindingContext = vmModule.mainViewModel;
+        	var resp = http.getJSON("https://challengeaccepted.azurewebsites.net/api/challenge/get").then(function (r) {
+						    	//challengesResponse = JSON.stringify(r);
+						    	challengesResponse = r;
+
+						    	console.log(r.length);
+
+	    	                    //changeContent();
+							}, function (e) {
+								console.log('Error getting challenges');
+							    console.log(e);
+							});
+            // page.bindingContext = pavmModule.mainViewModel;
             // topmost = frameModule.topmost();
         },
 
@@ -76,25 +112,29 @@ var challengeModules = (function() {
         	var i;
         	changeContent();
 
-            console.log('Title: ' + title);
-
             //TODO: Implement not repeating challenge in one loop
         }
     };
 
     function changeContent() {
-    	currentIndex = Math.floor((Math.random() * 3) + 1);
-        title = response[currentIndex]["Title"];        
+    	currentIndex = Math.floor(Math.random() * (challengesResponse.length - 1));
+
+        title = challengesResponse[currentIndex]["Title"];        
         titleLabel.text = title;
 
         // imageUrl = response[currentIndex]["ImageUrl"]
-        // image.src = imageUrl;
+        // image.imageSource = imageSource.fromUrl("https://www.google.com/images/errors/logo_sm_2.png")
 
-        description = response[currentIndex]["Description"];
+        description = challengesResponse[currentIndex]["Description"];
         descriptionLabel.text = description;
 
-        rating = response[currentIndex]["Difficulty"];
+        rating = challengesResponse[currentIndex]["Difficulty"];
         ratingLabel.text = 'Rating: ' + rating;
+
+        console.log('CurrentIndex: ' + currentIndex);
+        console.log('Index: ' + index + ' *** Title: ' + title);
+        console.log('-----------------');
+        index++;
     }
 
     return challengeModules;
